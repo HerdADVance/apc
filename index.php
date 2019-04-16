2fa424309e6cd0b31330022024f3507d6a54cf06
jnarcise <jnarcise@bulldogcreative.com>
2019-04-16 08:32:37 -0400

    Homepage progress

---
+++ index.php
@@ -0,0 +1,122 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="UTF-8">
+    <title>Appalachian Pest Control </title>
+    <link rel="icon" href="favicon.ico">
+
+    <meta name="author" content="Bulldog Creative Services">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+    <link href="/assets/css/normalize.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <link href="/assets/css/mobile.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,600,700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+  </head>
+
+  <body class="home">
+
+  	<header class="header">
+        <div class="header-top pri row unflex wrap-bg">
+            <div class="col-6 header-top-left">
+                <ul class="fa-ul ul-inline no-mar">
+                    <li class="top-phone"><a href="tel:1-304-522-7770"><span class="fa-li"></span><i class="fas fa-phone"></i>304.522.7770</a></li>
+                    <li class="top-email"><a href="mailto:info@appalachianpestcontrol.com"><span class="fa-li"></span><i class="fal fa-envelope"></i>info@appalachianpestcontrol.com</a></li>
+                </ul>
+            </div>
+            <div class="col-6 header-top-right">
+                <nav class="nav-inline">
+                    <ul class="fa-ul no-mar pull-right">
+                        <li class="search"><a href="#"><span class="fa-li"></span><i class="fal fa-search"></i>Search</a></li>
+                        <li><a class="block-link sec upper" href="/quote-request">Quote Request</a></li>
+                        <li><a href="https://www.facebook.com/AppalachianPestControl/" target="_BLANK"><span class="fa-li"></span><i class="fal fa-facebook"></i></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="header-bottom wrap-bg flex">
+            <div class="col-3 header-logo">
+      		    <a href="/"><img class="logo" src="/assets/images/logo.png" alt="Appalachian Pest Control. 304.522.7770"></a>
+            </div>
+            <div class="col-9 header-nav">
+                <nav class="nav nav-inline pull-right">
+                    <ul>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/residential-services">Residential Services</a></li>
+                        <li><a href="/commercial-services">Commercial Services</a></li>
+                        <li><a href="/pest-identifier">Pest Identifier</a></li>
+                        <li><a href="/testimonials">Testimonials</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>            
+                </nav>
+            </div>
+        </div>
+  	</header>
+
+  	<main class="home-main">
+
+        <section class="sliders no-pad-y bg-img" style="background-image:url(/assets/images/slider-tech.jpg);">
+            <div class="on-top wrap">
+                <p class="slider-title">Got Pests?</p>
+                <p class="slider-subtitle">You've come to the right place.</p>
+                <p>Here at Appalachian Pest Control, we offer effective, humane, and cost-friendly services to West Virginia, Ohio, and Kentucky.</p>
+                <ul>
+                    <li><a href="/our-company">Our Company</a></li>
+                    <li><a href="pest-identifier">Pest Identifier</a></li>
+                </ul>
+            </div>
+	  	</section>
+
+        <section class="wrap">
+            <p>Our certified, experienced, and effective team of pest control experts are at your service! With over 50 years of collective experience, we've seen it all when it comes to pests in your home or business.</p>
+            <p>Our team is quick, affordable and comprehensive. We understand what it takes to rid your commercial or residential property of pests, infestations and prevent more. We use only the most humane, safest and proven methods to eliminate pests and prevent more. We've been a trusted resource for pest control in West Virginia, Ohio, and Kentucky for decades. Our team is available for on-call emergency pest situations and provide you with a comprehensive review of our services, on-site and on-time. For the best in pest control, choose us today!</p>
+            <ul class="home-boxes row mar-middle">
+                <li class="col-6 bg-img" style="background-image: url(/assets/images/home-commercial.jpg);"><a class="block-link white-text upper" href="commercial-services">Commercial</a></li>
+                <li class="col-6 bg-img" style="background-image: url(/assets/images/home-residential.jpg);"><a class="block-link white-text upper" href="residential-services">Residential</a></li>
+            </ul>
+        </section>
+
+  	</main>
+
+  	<footer class="footer wrap-bg">
+        <div class="row">
+            <div class="col-6">
+                <p>With experienced technicians and industry-leading practices, we've got the solutions you need for your pest problem.</p>
+                <ul>
+                    <li><a href="">Employee Login</a></li>
+                    <li><a href="" target="_BLANK"><img src="/assets/images/npma-logo.jpg"></a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <nav>
+                    <ul>
+                        <li><a href="/residential-services">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/services">Services</a></li>
+                        <li><a href="/testimonials">Testimonials</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-3">
+                <p>Appalachian Pest Control<br/>
+                403 Adams Avenue<br/>
+                Huntington, WV 25701<br/>
+                <a href="tel:13045227770">(304) 522-7770</a><br/>
+                <a href="mailto:info@appalachianpestcontrol.com">info@appalachianpestcontrol.com</a></p>
+
+                <p>&copy; 2019 Appalachian Pest Control</p>
+            </div>
+        </div>
+    </footer>
+
+  	<script src="/assets/js/script.js"></script>
+
+  </body>
+
+</html>
